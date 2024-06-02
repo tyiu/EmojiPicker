bf9d0ee8906a719ac10b8f74aab05020f184394e
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-06-01 23:02:00 -0400

    Change Smile dependency to fork with updated emoji list

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/onmyway133/Smile", from: "2.1.0")
+        .package(url: "https://github.com/tyiu/Smile", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
